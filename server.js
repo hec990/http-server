452e662893c8979d8d4e7cc41977e7031f2b3b7a
xiaohh <1296522554@qq.com>
2022-01-19 17:03:26 +0800

    update server

--- server.js
+++ server.js
@@ -3,54 +3,58 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if (!port) {
-    console.log('��ָ���˿ںźò�����\nnode server.js 8888 ����������')
-    process.exit(1)
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
 }
 
-var server = http.createServer(function(request, response) {
-    var parsedUrl = url.parse(request.url, true)
-    var pathWithQuery = request.url
-    var queryString = ''
-    if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
-    var path = parsedUrl.pathname
-    var query = parsedUrl.query
-    var method = request.method
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
 
-    /******** �����￪ʼ�������治Ҫ�� ************/
+  /******** 从这里开始看，上面不要看 ************/
 
-    console.log('�и�ɵ�ӷ������������·��������ѯ������Ϊ��' + pathWithQuery)
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    // ���ʸ�Ŀ¼����Ӧ������
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write("后台服务器登录成功！")
-        response.end()
-    } else if (path === '/x') { // ���ʸ�Ŀ¼����ѯ����  ==> CSS code
-        response.statusCode = 200 // ״̬��
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`
-                        body { color: green; }
-                        `)
-        response.end()
-    } else if (path === '/y') { // ���ʸ�Ŀ¼����ѯ����  ==> JS code
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
-        response.write(`
-                        console.log('Hello JS')
-                        `)
-        response.end()
-    } else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`�������·�������
-                        ڶ� Ӧ������ `)
-        response.end()
-    }
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`
+     <!DOCTYPE html>
+     <head>
+        <link rel="stylesheet" href="/x"/>
+        <title>server</title>
+     </head> 
+     <body>
+       <h1>Hello</h1>
+       <script src="/y"></script>
+     </body>
+    `)
+    response.end()  // 调用end后，才会将响应内容发送到浏览器
+  } else if(path === '/x'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write(`body{color: red;}`)
+    response.end()
+  } else if(path === '/y'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write(`console.log("Hello JS")`)
+    response.end()
+  } else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`你输入的路径不存在对应的内容`)
+    response.end()
+  }
 
-    /******** ������������治Ҫ�� ************/
+  /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
-console.log('���� ' + port + ' �ɹ�\n�����ڿ���ת��720��Ȼ���õ緹�Ҵ� http://localhost:' + port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
